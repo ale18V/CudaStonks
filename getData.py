ae42861d5ec1c168f9034580653435b5a982b928
ale18V <alebombarda@icloud.com>
2024-05-30 09:36:36 -0700

    Change data from hourly to daily

--- getData.py
+++ getData.py
@@ -1,21 +1,21 @@
 import yfinance as yf
 import pandas as pd
-import datetime
+from datetime import datetime, timedelta
 import os
 
 tickers = ['AAPL', 'AMZN', 'TSLA', 'MSFT', 'GOOG']
 
-end_date = datetime.datetime.now().date()
-start_date = end_date - datetime.timedelta(days=5*365)
+end_date = datetime.now()
+start_date = end_date - timedelta(days=729)
 DATA_DIR = "data"
 
 
-def fetch_and_save_stock_data(ticker):
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    file_name = os.path.join(DATA_DIR, f'{ticker}_5_years_stock_data.csv')
+def fetch_and_save_stock_data(ticker, start_date, end_date, interval):
+    stock_data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    file_name = os.path.join(DATA_DIR, f'{ticker}_{interval}_data.csv')
     stock_data.to_csv(file_name)
     print(f'Saved {ticker} data to {file_name}')
 
 
 for ticker in tickers:
-    fetch_and_save_stock_data(ticker)
+    fetch_and_save_stock_data(ticker, start_date, end_date, '1h')
